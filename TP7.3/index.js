54fe5e041a5df2b8c95da2ffcb71ba1788429920
Aingsopheak <115513001+Aingsopheak@users.noreply.github.com>
2023-03-14 20:04:08 +0700

    Add files via upload

---
+++ TP7.3/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const fs = require('fs');
+
+const app = express()
+
+app.get('/', (req, res) => {
+  fs.readFile('./src/index.html', 'utf8', (err, data) => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+      return res.send("There is a server error");
+    }
+    res.send(data);
+  });
+})
+app.get('/detail', (req, res) => {
+  fs.readFile('./src/BookDetail.html', 'utf8', (err, data) => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.error(err);
+      return res.send("There is a server error");
+    }
+    res.send(data);
+  });
+})
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log("The server is running on port 3000");
+})
